41885cb3c32897ca601ba6f03cee147e6e138f01
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-10-06 10:52:24 -0500

    question base work

--- index.js
+++ index.js
@@ -4,14 +4,55 @@ const generateHTML = require('./src/GenerateHTML');
 const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
+const Employee = require('./lib/Employee');
 
 const fs = require('fs');
 const inquirer = require('inquirer');
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'Manager',
+        message: 'Are you the manager',
+
+    }
+]
+
+const menuQuestion = {
+    type: 'list',
+    name: 'addFinish',
+    message: 'What would you like to do next',
+    choices: ['Add employee', 'Finish HTML page']
+
+}
 
 const teamArray = [];
 
-const addManager = () => {}
+const promptMenu = () => {
+inquirer.prompt(menuQuestion).then(answer => {
+
+    if(answer.addFinish === 'Add employee'){
+        addEmployee()
+    }else{
+        writeFile()
+    }
+
+})
+}
+
+
+const addManager = () => {
+    inquirer.prompt(managerQuestions).then(managerInfo => {
+        console.log(managerInfo)
+        promptMenu()
+    }) 
+}
 
 const addEmployee = () => {}
 
 const writeFile = data => {}
+
+const employee1 = new Employee('bryce',45, 'lol@gmail.com')
+console.log(employee1.name)
+console.log(employee1.sayHi())
+
+addManager()
