255249fd9a327d22bec0ba268d5d5d130e2f64ce
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-10-05 19:50:59 -0500

    finished fille set up skeleton of page framworls and HTML page skelton to generate

---
+++ Tests/Intern.test.js
@@ -0,0 +1,1 @@
+const Itern = require('../lib/Intern');
---
+++ index.js
@@ -0,0 +1,17 @@
+//Linking all pages for page creation and team profiles
+const generateHTML = require('./src/GenerateHTML');
+
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const teamArray = [];
+
+const addManager = () => {}
+
+const addEmployee = () => {}
+
+const writeFile = data => {}
